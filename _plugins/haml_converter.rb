b73fd1f1e2f97f1825f9b3f1e6a0d56396d8d9af
Ian Lollar <ian.lollar@gmail.com>
2012-07-02 00:48:41 -0700

    Initial commit

---
+++ _plugins/haml_converter.rb
@@ -0,0 +1,25 @@
+module Jekyll
+  require 'haml'
+
+  class HamlConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /haml/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      begin
+        engine = Haml::Engine.new(content)
+        engine.render
+      rescue StandardError => e
+        puts e.message
+      end
+    end
+  end
+end
