b9919d109117475e73eafab121f5454ac30342f3
Ian Lollar <ian.lollar@gmail.com>
2012-07-08 14:58:11 -0700

    Change to pretty perma's - mod Sinatra to support

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require 'sinatra'
 set :public_folder, File.dirname(__FILE__) + '/public'
 set :static, true
 
-get "*" do
-    send_file File.join(settings.public_folder, 'index.html')
+get '*' do
+  send_file File.join(settings.public_folder, request.path, 'index.html')
+end
+
+not_found do
+  send_file File.join(settings.public_folder, '404', 'index.html')
 end
